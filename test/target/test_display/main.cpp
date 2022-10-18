0f4e7b937591f25e080155b69daee78c0bc2bc5f
Strooom <pascal.roobrouck@gmail.com>
2022-10-18 15:07:21 +0200

    set up test framework

--- test/target/test_display/main.cpp
+++ test/target/test_display/main.cpp
@@ -1,15 +1,12 @@
 #include <Arduino.h>
-#include "encoderwheel.h"
+#include "unity.h"
 #include "HD44780.h"
 
-
-encoderWheel theWheel;
 HD44780 theDisplay(displayType::Type20X4, 0x28);
 
 void setup() {
-  theDisplay.initialize();
+    theDisplay.initialize();
 }
 
 void loop() {
-  
 }
--- test/target/test_encoder/main.cpp
+++ test/target/test_encoder/main.cpp
@@ -1,15 +1,11 @@
 #include <Arduino.h>
+#include "unity.h"
 #include "encoderwheel.h"
-#include "HD44780.h"
-
 
 encoderWheel theWheel;
-HD44780 theDisplay(displayType::Type20X4, 0x28);
 
 void setup() {
-  theDisplay.initialize();
 }
 
 void loop() {
-  
 }
