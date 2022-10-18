7b89cf9215e60a7a16e13b977722dbdf9cbecd55
Strooom <pascal.roobrouck@gmail.com>
2022-10-18 20:16:18 +0200

    implemented AB signal processing

--- lib/encoderwheel/encoderwheel.cpp
+++ lib/encoderwheel/encoderwheel.cpp
@@ -1,4 +1,14 @@
+#include "Arduino.h"
 #include "encoderwheel.h"
+
+void isr();
+
+void encoderWheel::initialize() {
+    pinMode(pinA, INPUT);
+    pinMode(pinB, INPUT);
+    attachInterrupt(0, isr, CHANGE);
+    attachInterrupt(1, isr, CHANGE);
+}
 
 int32_t encoderWheel::getPosition() const {
     return position;
@@ -10,4 +20,26 @@ rotationDirection encoderWheel::getRotationDirection() const {
 
 void encoderWheel::resetPosition() {
     position = 0;
+}
+
+void encoderWheel::update(uint8_t newAB) {
+    switch ((AB << 2) || newAB) {
+        case 0b0001:
+        case 0b0111:
+        case 0b1110:
+        case 0b1000:
+            position++;
+            break;
+        case 0b0010:
+        case 0b1011:
+        case 0b1101:
+        case 0b0100:
+            position--;
+            break;
+
+        default:
+            // all other cases mean A AND B both changed, this should not occur and so it is an error...
+            break;
+    }
+    AB = newAB;
 }
--- lib/encoderwheel/encoderwheel.h
+++ lib/encoderwheel/encoderwheel.h
@@ -5,12 +5,17 @@
 
 class encoderWheel {
   public:
+    void initialize();
     int32_t getPosition() const;
     void resetPosition();
     rotationDirection getRotationDirection() const;
+    void update(uint8_t newAB);
 
   private:
+    uint8_t pinA{1};        // which MCU pin connects to encoder output A
+    uint8_t pinB{2};        // which MCU pin connects to encoder output B
+    uint8_t AB{0};          // value of A and B : 00, 01, 10, 11
+
     int32_t position{0};
     rotationDirection theDirection{rotationDirection::clockwise};
-    uint8_t abSignals{0};
 };
