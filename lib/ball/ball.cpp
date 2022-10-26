7595730dfb21a800562026c7a1ab09563df86c35
Strooom <pascal.roobrouck@gmail.com>
2022-10-26 12:59:42 +0200

    extend to more than 2x2 chars

--- lib/ball/ball.cpp
+++ lib/ball/ball.cpp
@@ -1,4 +1,5 @@
 #include "ball.h"
+#include "HD44780.h"
 
 float ball::getXPosition() const {
     return xPosition;
@@ -27,6 +28,13 @@ void ball::setTokensFromPixelCoordinates(uint8_t x, uint8_t y) {
     clearTokens();
     char bitsLeft{0};
     char bitsRight{0};
+
+    uint8_t col = getLeftColFromPixelCoordinate(x);
+    uint8_t row = getBottomRowFromPixelCoordinate(y);
+
+    x = x - getPixelOffsetFromCol(col);
+    y = y - getPixelOffsetFromRow(row);
+
     if (x == 0) {
         bitsLeft = 0b11000;
     } else if (x == 1) {
@@ -53,138 +61,141 @@ void ball::setTokensFromPixelCoordinates(uint8_t x, uint8_t y) {
     }
 
     if (y == 0) {
-        bottomLeft[6] = bitsLeft;
+        bottomLeft[6]  = bitsLeft;
         bottomRight[6] = bitsRight;
-        bottomLeft[7] = bitsLeft;
+        bottomLeft[7]  = bitsLeft;
         bottomRight[7] = bitsRight;
     } else if (y == 1) {
-        bottomLeft[5] = bitsLeft;
+        bottomLeft[5]  = bitsLeft;
         bottomRight[5] = bitsRight;
-        bottomLeft[6] = bitsLeft;
+        bottomLeft[6]  = bitsLeft;
         bottomRight[6] = bitsRight;
-        bottomLeft[7] = bitsLeft;
+        bottomLeft[7]  = bitsLeft;
         bottomRight[7] = bitsRight;
     } else if (y == 2) {
-        bottomLeft[4] = bitsLeft;
+        bottomLeft[4]  = bitsLeft;
         bottomRight[4] = bitsRight;
-        bottomLeft[5] = bitsLeft;
+        bottomLeft[5]  = bitsLeft;
         bottomRight[5] = bitsRight;
-        bottomLeft[6] = bitsLeft;
+        bottomLeft[6]  = bitsLeft;
         bottomRight[6] = bitsRight;
     } else if (y == 3) {
-        bottomLeft[3] = bitsLeft;
+        bottomLeft[3]  = bitsLeft;
         bottomRight[3] = bitsRight;
-        bottomLeft[4] = bitsLeft;
+        bottomLeft[4]  = bitsLeft;
         bottomRight[4] = bitsRight;
-        bottomLeft[5] = bitsLeft;
+        bottomLeft[5]  = bitsLeft;
         bottomRight[5] = bitsRight;
     } else if (y == 4) {
-        bottomLeft[2] = bitsLeft;
+        bottomLeft[2]  = bitsLeft;
         bottomRight[2] = bitsRight;
-        bottomLeft[3] = bitsLeft;
+        bottomLeft[3]  = bitsLeft;
         bottomRight[3] = bitsRight;
-        bottomLeft[4] = bitsLeft;
+        bottomLeft[4]  = bitsLeft;
         bottomRight[4] = bitsRight;
     } else if (y == 5) {
-        bottomLeft[1] = bitsLeft;
+        bottomLeft[1]  = bitsLeft;
         bottomRight[1] = bitsRight;
-        bottomLeft[2] = bitsLeft;
+        bottomLeft[2]  = bitsLeft;
         bottomRight[2] = bitsRight;
-        bottomLeft[3] = bitsLeft;
+        bottomLeft[3]  = bitsLeft;
         bottomRight[3] = bitsRight;
     } else if (y == 6) {
-        bottomLeft[0] = bitsLeft;
+        bottomLeft[0]  = bitsLeft;
         bottomRight[0] = bitsRight;
-        bottomLeft[1] = bitsLeft;
+        bottomLeft[1]  = bitsLeft;
         bottomRight[1] = bitsRight;
-        bottomLeft[2] = bitsLeft;
+        bottomLeft[2]  = bitsLeft;
         bottomRight[2] = bitsRight;
     } else if (y == 7) {
-        bottomLeft[0] = bitsLeft;
+        bottomLeft[0]  = bitsLeft;
         bottomRight[0] = bitsRight;
-        bottomLeft[1] = bitsLeft;
+        bottomLeft[1]  = bitsLeft;
         bottomRight[1] = bitsRight;
     } else if (y == 8) {
-        topLeft[7] = bitsLeft;
-        topRight[7] = bitsRight;
-        bottomLeft[1] = bitsLeft;
+        topLeft[7]     = bitsLeft;
+        topRight[7]    = bitsRight;
+        bottomLeft[1]  = bitsLeft;
         bottomRight[1] = bitsRight;
     } else if (y == 9) {
-        topLeft[6] = bitsLeft;
+        topLeft[6]  = bitsLeft;
         topRight[6] = bitsRight;
-        topLeft[7] = bitsLeft;
+        topLeft[7]  = bitsLeft;
         topRight[7] = bitsRight;
     } else if (y == 10) {
-        topLeft[5] = bitsLeft;
+        topLeft[5]  = bitsLeft;
         topRight[5] = bitsRight;
-        topLeft[6] = bitsLeft;
+        topLeft[6]  = bitsLeft;
         topRight[6] = bitsRight;
-        topLeft[7] = bitsLeft;
+        topLeft[7]  = bitsLeft;
         topRight[7] = bitsRight;
     } else if (y == 11) {
-        topLeft[4] = bitsLeft;
+        topLeft[4]  = bitsLeft;
         topRight[4] = bitsRight;
-        topLeft[5] = bitsLeft;
+        topLeft[5]  = bitsLeft;
         topRight[5] = bitsRight;
-        topLeft[6] = bitsLeft;
+        topLeft[6]  = bitsLeft;
         topRight[6] = bitsRight;
     } else if (y == 12) {
-        topLeft[3] = bitsLeft;
+        topLeft[3]  = bitsLeft;
         topRight[3] = bitsRight;
-        topLeft[4] = bitsLeft;
+        topLeft[4]  = bitsLeft;
         topRight[4] = bitsRight;
-        topLeft[5] = bitsLeft;
+        topLeft[5]  = bitsLeft;
         topRight[5] = bitsRight;
     } else if (y == 13) {
-        topLeft[2] = bitsLeft;
+        topLeft[2]  = bitsLeft;
         topRight[2] = bitsRight;
-        topLeft[3] = bitsLeft;
+        topLeft[3]  = bitsLeft;
         topRight[3] = bitsRight;
-        topLeft[4] = bitsLeft;
+        topLeft[4]  = bitsLeft;
         topRight[4] = bitsRight;
     } else if (y == 14) {
-        topLeft[1] = bitsLeft;
+        topLeft[1]  = bitsLeft;
         topRight[1] = bitsRight;
-        topLeft[2] = bitsLeft;
+        topLeft[2]  = bitsLeft;
         topRight[2] = bitsRight;
-        topLeft[3] = bitsLeft;
+        topLeft[3]  = bitsLeft;
         topRight[3] = bitsRight;
     } else if (y == 15) {
-        topLeft[0] = bitsLeft;
+        topLeft[0]  = bitsLeft;
         topRight[0] = bitsRight;
-        topLeft[1] = bitsLeft;
+        topLeft[1]  = bitsLeft;
         topRight[1] = bitsRight;
-        topLeft[2] = bitsLeft;
+        topLeft[2]  = bitsLeft;
         topRight[2] = bitsRight;
     } else if (y == 16) {
-        topLeft[0] = bitsLeft;
+        topLeft[0]  = bitsLeft;
         topRight[0] = bitsRight;
-        topLeft[1] = bitsLeft;
+        topLeft[1]  = bitsLeft;
         topRight[1] = bitsRight;
     }
 }
 
-
-uint8_t ball::getColFromPixelCoordinate(uint8_t pixelX) {
-    if (pixelX < 10) {
+uint8_t ball::getLeftColFromPixelCoordinate(uint8_t x) {
+    if (x <= 9) {
         return 0;
     }
-    if (pixelX > 118) {
-        return 19;
+    if (x > xMax) {
+        x = xMax;
     }
-    return ((pixelX - 10) / 6) + 1;
+    return ((x - 10) / 6) + 1;
 }
 
-
-uint8_t ball::getRowFromPixelCoordinate(uint8_t pixelY) {
-    if (pixelY <= 15) {
+uint8_t ball::getBottomRowFromPixelCoordinate(uint8_t y) {
+    if (y <= 15) {
         return 3;
     }
-    if (pixelY <= 25) {
+    if (y <= 23) {
         return 2;
     }
-    if (pixelY <= 35) {
-        return 1;
-    }
-    return 0;
+    return 1;
+}
+
+uint8_t ball::getPixelOffsetFromCol(uint8_t col) {
+    return (col * (HD44780::horizontalPixelsPerCharacter + HD44780::pixelsBetweenCharacters));
+}
+
+uint8_t ball::getPixelOffsetFromRow(uint8_t row) {
+    return ((3-row) * (HD44780::verticalPixelsPerCharacter + HD44780::pixelsBetweenCharacters));
 }
--- lib/ball/ball.h
+++ lib/ball/ball.h
@@ -7,13 +7,23 @@ class ball {
     float getYPosition() const;
     void updatePosition();
 
-    char topLeft[8]{0};                                  // 4 custom characters are used to draw a 3*3 pixel ball
+    char topLeft[8]{0};        // 4 custom characters are used to draw a 3*3 pixel ball
     char topRight[8]{0};
     char bottomLeft[8]{0};
     char bottomRight[8]{0};
 
     void setTokensFromPixelCoordinates(uint8_t x, uint8_t y);
-  
+
+    static constexpr uint8_t xMin{1};         // minimum X-coordinate of the ball, as ball is 3*3 pixels, the center is minimum at x = 1
+    static constexpr uint8_t xMax{45};        // maximum X-coordinate of the ball : 8 chars with 5 pixels each, 1 pixel between chars 7 * (5+1) + 5 = 47, index is 0-46, 3*3 px so max index is 45
+    static constexpr uint8_t yMin{1};         // minimum Y-coordinate of the ball, as ball is 3*3 pixels, the center is minimum at y = 1
+    static constexpr uint8_t yMax{33};        // maximum Y-coordinate of the ball : 4 chars with 8 pixels each, 1 pixel between chars 3 * (8+1) + 8 = 35, index is 0-34, 3*3 px so max index is 33
+
+    uint8_t getBottomRowFromPixelCoordinate(uint8_t pixelY);        // translate a display pixel coordinate x into character coordinate = column
+    uint8_t getLeftColFromPixelCoordinate(uint8_t pixelX);          // translate a display pixel coordinate y into character coordinate = row
+    uint8_t getPixelOffsetFromCol(uint8_t col);
+    uint8_t getPixelOffsetFromRow(uint8_t row);
+
   private:
     float xPosition{0};
     float yPosition{0};
@@ -21,6 +31,5 @@ class ball {
     float ySpeed{0};
 
     void clearTokens();
-    uint8_t getRowFromPixelCoordinate(uint8_t pixelY);        // translate a display pixel coordinate x into character coordinate = column
-    uint8_t getColFromPixelCoordinate(uint8_t pixelX);        // translate a display pixel coordinate y into character coordinate = row
+
 };
